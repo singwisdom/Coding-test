a48a6ce6c34625f23c4ee3567fc1f3db92e4d6a9
singwisdom <57509973+singwisdom@users.noreply.github.com>
2024-05-08 09:58:12 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,078 ms, Memory: 51,472 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,9 +1,9 @@
 # SWEA 6190번 : 정곤이의 단조 증가하는 수
-# 자료구조 & 알고리즘 :
+# 자료구조 & 알고리즘 : 문제 이해 못해서 시간 오래 걸림
 
 def is_up(num):
     res = 0
-    num_list = list(str(num))
+    num_list = list(str(num)) # 여기는 내 생각으로 안 품 , 구글링해서 품
     for k in range(len(num_list)-1):
         if int(num_list[k]) > int(num_list[k+1]) : 
             res = False
@@ -17,15 +17,12 @@ for t in range(1,tc+1):
 
     n = int(input()) # 숫자 자리수
     num = list(map(int, input().split()))
-    max_num = 0
+    result = -1
 
     for i in range(n): # Ai * Aj 구하기
         for j in range(i+1,n) :
             calc = num[i] * num[j]
-            if calc > max_num : # 단조 증가하는 수인지 판별
-                result = is_up(calc)
-                if result == True : 
-                    max_num = max(calc, max_num)
+            if calc > result and is_up(calc) : # 단조 증가하는 수인지 판별
+                result = calc             
 
-    if max_num <= 0 :print(f'#{t} -1')
-    else :print(f'#{t} {max_num}') # 단조 증가하는 수 중, 최댓값 판별
+    print(f'#{t} {result}') # 단조 증가하는 수 중, 최댓값 판별
