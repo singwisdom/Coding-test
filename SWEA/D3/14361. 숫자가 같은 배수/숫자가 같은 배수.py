a112a1504e6899e37ccc7cd9556691c2e3f6cda5
singwisdom <57509973+singwisdom@users.noreply.github.com>
2024-05-16 13:39:30 +0900

    [D3] Title: 숫자가 같은 배수, Time: 139 ms, Memory: 49,476 KB -BaekjoonHub

---
+++ SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.py
@@ -0,0 +1,22 @@
+# SWEA 14361번 : 숫자가 같은 배수
+# 알고리즘 & 자료구조 : 숫자가 7자리가 최대니, 순열로 풀어서 완탐하면 될 것 같음
+
+from itertools import permutations
+
+def is_possible(permu, num_list):
+    for i in range(len(permu)):
+        if permu[i][0] > num_list[0] : # 2배수부터 계산하는 것이므로 (순열 리스트 앞 자리가 원래 숫자 첫번째 자리보다 작으면 비교 안해도 됨)
+            tmp = ''
+            for j in range(len(permu[i])) :
+                tmp += permu[i][j] # 순열을 다 더해준다.
+            if int(tmp) % num == 0 : return 'possible'
+
+tc = int(input())
+for t in range(1,tc+1):
+    num = int(input())
+    num_list = list(str(num)) # 배열에 각 숫자 저장
+    permu = list(permutations(num_list, len(num_list)))
+
+    res = is_possible(permu, num_list)
+    if res == 'possible' : print(f'#{t} {res}')
+    else : print(f'#{t} impossible')
