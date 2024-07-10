a5cd63c99a13351d634d95ad619cecc2f0c183a4
singwisdom <57509973+singwisdom@users.noreply.github.com>
2024-07-10 21:46:11 +0900

    [D1] Title: 서랍의 비밀번호, Time: 133 ms, Memory: 26,952 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+			Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(p-k+1); 
+	}
+}
