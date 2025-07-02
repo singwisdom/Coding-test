0580b235e94a18a3db00456c64bebf149fcdcd7f
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-07-02 10:00:58 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 27632 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+ import java.io.*;
+ import java.util.*;
+ 
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num ==0) {
+				if(queue.size()==0) {
+					bw.write("0\n");
+				} else {
+					bw.write(queue.poll()+"\n");
+				}
+			} else {
+				queue.add(num);
+			}
+		}
+		bw.flush();
+
+	}
+}
