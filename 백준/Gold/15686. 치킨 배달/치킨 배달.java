7286ff8937c19f4480f57a75f9f1fbf4890dde2c
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-10-17 23:07:02 +0900

    [Gold V] Title: 치킨 배달, Time: 176 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -3,35 +3,36 @@ import java.util.*;
 
 public class Main {
 
-	static int res = Integer.MAX_VALUE;
-	static int N, M;
-	static int[][] map, chicken;
-	static int[][] sel;
-	public static void main(String[] args) throws IOException{
+	static int[][] map, chicken, sel;
+	static int N,M, result;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-
+		result = Integer.MAX_VALUE;
 		int cnt = 0;
 		
-		for (int i = 0; i < N; i++) {
+		map = new int[N][N];
+		
+		// 맵 입력받기 
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+			for (int j=0; j<N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if(map[i][j]==2) cnt+=1;
 			}
 		}
-
-		// 치킨집 구하기
+		
+		// 치킨집 구하기 (치킨집 좌표 저장)
 		chicken = new int[cnt][2];
 		int tmp = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(map[i][j] == 2) {
+		
+		for(int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				if(map[i][j]==2) {
 					chicken[tmp][0] = i;
 					chicken[tmp][1] = j;
 					tmp+=1;
@@ -39,36 +40,38 @@ public class Main {
 			}
 		}
 		
-		// 조합 구하기
-		for (int i = 0; i <= M; i++) {
+		
+		// 조합 구하기 		
+		for (int i = 1; i <= M; i++) {
 			sel = new int[M][2];
 			combination(0,0);
+			
 		}
 		
-		bw.write(res+"\n");
-		bw.flush();
+		bw.write(result+"\n");
+		bw.close();
+	
 	}
 	
 	// idx : 원본배열의 순서값, k : 선택한 갯수
 	private static void combination(int idx, int k) {
-
-		if(k == sel.length) {
-			int tmpMin = 0;
-			int min = 0;
-			// 각 집마다 치킨거리 구하기
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if(map[i][j]==1) {
-						tmpMin = Integer.MAX_VALUE;
-						for (int num = 0; num < sel.length; num++) {
-							tmpMin = Math.min(tmpMin, Math.abs(sel[num][0]-i) + Math.abs(sel[num][1]-j));
+		
+		if(k==sel.length) {
+			// 치킨거리 구하기
+			int tmpResult = 0;
+			for(int r=0; r<N; r++) {
+				for (int c=0; c<N; c++) {
+					// 집을 발견
+					if(map[r][c]==1) {
+						int tmpLength = Integer.MAX_VALUE;
+						for (int i = 0; i < sel.length; i++) {
+							tmpLength = Math.min(Math.abs(sel[i][0] - r) + Math.abs(sel[i][1] - c), tmpLength);
 						}
-						min += tmpMin;
+						tmpResult += tmpLength;
 					}
-					
 				}
 			}
-			res = Math.min(res, min);
+			result = Math.min(result, tmpResult);
 			return;
 		}
 		
@@ -81,6 +84,7 @@ public class Main {
 		
 		// 선택하지 않는 경우
 		combination(idx+1, k);
+		
 	}
 
 }
